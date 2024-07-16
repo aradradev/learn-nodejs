6121e60ccc545e839fba64b94047199e58d46133
Abdoul <abdoulramanediallo44@gmail.com>
2024-07-16 09:16:27 +0000

    learn params query and destructured a chunk of data

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const { products } = require('./data')
-
 const app = express()
 
 app.get('/', (req, res) => {
-  res.json(products)
+  res.send('<h1>Home Page</h1> <a href="/api/products">products</a>')
+})
+app.get('/api/products', (req, res) => {
+  const newProducts = products.map((product) => {
+    const { id, name, image } = product
+    return { id, name, image }
+  })
+  res.json(newProducts)
+})
+
+app.all('*', (req, res) => {
+  res.send('<h3>resource not found</h3>')
 })
 
 app.listen(5000, () => {
