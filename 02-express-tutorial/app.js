e691e0cf6293ad487c365ff7ae639d418f2ed7d0
Abdoul <abdoulramanediallo44@gmail.com>
2024-07-16 19:10:49 +0000

    ad post method with html form

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const { people } = require('./data')
 const app = express()
+app.use(express.urlencoded({ extended: false }))
 
 app.use(express.static('./methods-public'))
-app.get('/api/v1/people', (req, res) => res.status(200).send({ status: true, data: people }))
-app.get('*', (req, res) => res.status(404).send({ status: false, message: 'resource not found' }))
+app.post('/login', (req, res) => {
+  const { name } = req.body
+  name ? res.status(200).send(`Welcome ${name}`) : res.status(401).send('Please provide a credential name')
+})
+
+app.get('/api/people', (req, res) => res.status(200).json({ status: true, data: people }))
+app.get('*', (req, res) => res.status(404).json({ status: false, message: 'resource not found' }))
 app.listen(5000, () => console.log('Server is listening on port 5000...'))
