f94cfca32371733258346051c6cd9da9385972c3
Abdoul <abdoulramanediallo44@gmail.com>
2024-07-16 10:56:44 +0000

    manipulate middleware with app.use

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,20 +1,15 @@
 const express = require('express')
+const logger = require('./logger')
 
 const app = express()
+// req => middleware => res
+app.use(logger)
 
-const logger = (req, res, next) => {
-  const method = req.method
-  const url = req.url
-  const time = new Date().getFullYear()
-  console.log(method, url, time)
-  next()
-}
-
-app.get('/', logger, (req, res) => {
+app.get('/', (req, res) => {
   res.send('Home page')
 })
 
-app.get('/about', logger, (req, res) => {
+app.get('/about', (req, res) => {
   res.send('About page')
 })
 
