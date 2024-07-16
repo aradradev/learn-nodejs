e5944a9ae37910d9c7fdf608c14627700f0c309e
Abdoul <abdoulramanediallo44@gmail.com>
2024-07-16 12:45:22 +0000

    add method get

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,19 +1,13 @@
 const express = require('express')
-const logger = require('./logger')
-const authorize = require('./authorize')
+const { people } = require('./data')
+
 const app = express()
-
-app.use([logger, authorize])
-app.get('/', (req, res) => {
-  console.log(req.user)
-  res.send('Home Page')
+app.get('/', (req, res) => res.send('<h2>Home Page</h2> <a href="/api/v1/people">people api</a>'))
+app.get('/api/v1/people', (req, res) => {
+  res.status(200).json({ status: true, data: people })
 })
-app.get('/about', (req, res) => {
-  res.send('About Page')
+app.all('*', (req, res) => {
+  res.status(404).json({ status: false, message: 'Not found' })
 })
 
-app.all('*', (req, res) => res.status(404).send('resource not found'))
-
-app.listen(5000, () => {
-  console.log('Server is listening on port 5000...')
-})
+app.listen(5000, () => console.log('Server is listening on port 5000...'))
