763691fb3f91c4d1df60bd2c7f936c0bf95afeb5
Abdoul <abdoulramanediallo44@gmail.com>
2024-07-16 18:25:33 +0000

    ad post method with external static data with middleware

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,13 +1,8 @@
 const express = require('express')
 const { people } = require('./data')
-
 const app = express()
-app.get('/', (req, res) => res.send('<h2>Home Page</h2> <a href="/api/v1/people">people api</a>'))
-app.get('/api/v1/people', (req, res) => {
-  res.status(200).json({ status: true, data: people })
-})
-app.all('*', (req, res) => {
-  res.status(404).json({ status: false, message: 'Not found' })
-})
 
+app.use(express.static('./methods-public'))
+app.get('/api/v1/people', (req, res) => res.status(200).send({ status: true, data: people }))
+app.get('*', (req, res) => res.status(404).send({ status: false, message: 'resource not found' }))
 app.listen(5000, () => console.log('Server is listening on port 5000...'))
