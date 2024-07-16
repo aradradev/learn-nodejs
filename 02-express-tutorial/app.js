718096654686d976c5b14cbd274bbb3c84420312
Abdoul <abdoulramanediallo44@gmail.com>
2024-07-16 08:43:45 +0000

    using the static public use middleware to dump the public assets

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,13 +1,12 @@
 const express = require('express')
-const path = require('path')
 
 const app = express()
 
 app.use(express.static('./public'))
 
-app.get('/', (_req, res) => {
-  res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
-})
+// app.get('/', (_req, res) => {
+//   res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+// })
 
 app.all('*', (_req, res) => {
   res.status(404).send('<h3>resource not found</h3>')
