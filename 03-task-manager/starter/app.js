2a796d49c9556355884bdae4672a6c66a02be303
Abdoul <abdoulramanediallo44@gmail.com>
2024-07-17 18:01:08 +0000

    adding the basic setup for the task manager app

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,15 @@
-console.log('Task Manager App')
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/hello', (req, res) => {
+  res.status(200).send('Task Manager App')
+})
+
+app.all('*', (req, res) => {
+  res.status(404).send('resource not found')
+})
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}...`)
+})
