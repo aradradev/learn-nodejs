b6ec7fefa00bf8585cf96c5af7d63123ca455435
Abdoul <abdoulramanediallo44@gmail.com>
2024-07-17 13:04:06 +0000

    add delete method

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -38,6 +38,18 @@ app.put('/api/people/:id', (req, res) => {
   res.status(200).json({ success: true, person: newPeople })
 })
 
+app.delete('/api/people/:id', (req, res) => {
+  const { id } = req.params
+  // console.log(`delete id ${id}`)
+  const peopleMatch = people.find((person) => person.id === Number(id))
+  if (!peopleMatch) {
+    return res.status(404).json({ success: false, msg: `person not found with id ${id}` })
+  }
+  const newPeople = people.filter((person) => person.id !== Number(id))
+  // console.log(newPeople)
+  return res.status(200).json({ success: true, data: newPeople })
+})
+
 app.get('/api/people', (req, res) => res.status(200).json({ status: true, data: people }))
 app.all('*', (req, res) => res.status(404).json({ status: false, message: 'resource not found' }))
 app.listen(5000, () => console.log('Server is listening on port 5000...'))
