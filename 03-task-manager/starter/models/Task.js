811025aee23a1a0be952caa0a367b5d0cc02280a
Abdoul <abdoulramanediallo44@gmail.com>
2024-07-18 21:29:23 +0000

    add try catch for handling errors

--- 03-task-manager/starter/models/Task.js
+++ 03-task-manager/starter/models/Task.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const TaskSchema = new mongoose.Schema({
   name: {
     type: String,
+    trim: true,
     required: [true, 'must provide a name'],
     maxlength: [20, 'name cannot be more than 20 characters'],
   },
