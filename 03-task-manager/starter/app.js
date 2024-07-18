ae94cc1a4458338953a213486af4ddd2f3c04a27
Abdoul <abdoulramanediallo44@gmail.com>
2024-07-18 08:04:28 +0000

    add the mongoose seperate file

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const tasks = require('./routes/tasks')
-require('./db/connect')
+const connectDB = require('./db/connect')
 const app = express()
 
 // middleware
@@ -11,14 +11,17 @@ app.use('/api/v1/tasks', tasks)
 //port
 const PORT = 3000
 
-app.get('/hello', (req, res) => {
-  res.status(200).send('Task Manager App')
-})
-
 app.all('*', (req, res) => {
   res.status(404).send('resource not found')
 })
 
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}...`)
-})
+const start = async () => {
+  try {
+    await connectDB()
+    app.listen(PORT, console.log(`Server is listening on port ${PORT}...`))
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+start()
--- 03-task-manager/starter/db/connect.js
+++ 03-task-manager/starter/db/connect.js
@@ -3,12 +3,16 @@ const mongoose = require('mongoose')
 const connectionString =
   'mongodb+srv://aradradev:1PiHuus31rV9NMis@nodeexpress.tqcui4j.mongodb.net/TASK-MANAGER?retryWrites=true&w=majority&appName=NodeExpress'
 
-mongoose
-  .connect(connectionString, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-  })
-  .then(() => console.log('CONNECTED TO THE DB...'))
-  .catch((err) => console.log(err))
+const connectDB = (url) => {
+  mongoose
+    .connect(connectionString, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    })
+    .then(() => console.log('DB CONNECTED...'))
+    .catch((err) => console.log(err))
+}
+
+module.exports = connectDB
