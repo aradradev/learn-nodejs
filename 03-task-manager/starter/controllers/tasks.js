768ab58a650adec089a75e9bf477af2756a8a657
Abdoul <abdoulramanediallo44@gmail.com>
2024-07-18 09:02:45 +0000

    add basics validation

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -5,7 +5,7 @@ const getAllTasks = (req, res) => {
 }
 const createTask = async (req, res) => {
   const task = await Task.create(req.body)
-  res.status(201).json(task)
+  res.status(201).json({ task })
 }
 const getTask = (req, res) => {
   res.send('Get a single task')
