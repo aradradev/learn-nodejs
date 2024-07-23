ca194605f3f9bc27013563c319a9990654ed43f7
Abdoul <abdoulramanediallo44@gmail.com>
2024-07-23 11:54:37 +0000

    add name

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -6,13 +6,16 @@ const getAllProductsStatic = async (req, res) => {
 }
 
 const getAllProducts = async (req, res) => {
-  const { featured, company } = req.query
+  const { featured, company, name } = req.query
   const queryObject = {}
   if (featured) {
     queryObject.featured = featured === 'true' ? true : false
   }
   if (company) {
     queryObject.company = company
+  }
+  if (name) {
+    queryObject.name = name
   }
   console.log(queryObject)
   const products = await Product.find(queryObject)
