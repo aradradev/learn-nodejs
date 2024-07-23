71f7a8104ff617c38194d1bed0faf1100b222076
Abdoul <abdoulramanediallo44@gmail.com>
2024-07-23 09:06:26 +0000

    fixing basic middleware

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -7,12 +7,12 @@ const app = express()
 const port = process.env.PORT || 3000
 // middleware
 app.use(express.json())
-app.use(notFoundMiddleware)
-app.use(errorHandlerMiddleware)
 
 app.get('/', (req, res) => {
   res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>')
 })
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
 
 const start = async () => {
   try {
