afb3250b90a5fd791df2d32dae6b7ca86c5fadd9
Abdoul <abdoulramanediallo44@gmail.com>
2024-07-23 09:06:37 +0000

    fixing basic error

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -11,6 +11,7 @@ app.use(express.json())
 app.get('/', (req, res) => {
   res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>')
 })
+
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
 
