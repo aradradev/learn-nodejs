2e2d548e022f844d6a56e8f45290afc7abe89916
Abdoul <abdoulramanediallo44@gmail.com>
2024-07-23 10:56:22 +0000

    handle error in passing wrong property

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -6,7 +6,13 @@ const getAllProductsStatic = async (req, res) => {
 }
 
 const getAllProducts = async (req, res) => {
-  const products = await Product.find(req.query)
+  const { featured } = req.query
+  const queryObject = {}
+  if (featured) {
+    return (queryObject.featured = featured === 'true' ? true : false)
+  }
+  console.log(queryObject)
+  const products = await Product.find(queryObject)
   res.status(200).json({ products, nbHits: products.length })
 }
 
