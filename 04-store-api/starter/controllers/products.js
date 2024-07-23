7a02fa716802a9e1de23f672c984d1b305f881d9
Abdoul <abdoulramanediallo44@gmail.com>
2024-07-23 10:34:23 +0000

    add static filter

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,11 +1,11 @@
 const Product = require('../models/product')
 
 const getAllProducts = async (req, res) => {
-  const products = await Product.find({ featured: true, company: 'caressa' })
-  res.status(200).json({ products, nbHits: products.length })
+  res.status(200).json({ msg: 'products testing route' })
 }
 const getAllProductsStatic = async (req, res) => {
-  res.status(200).json({ msg: 'products testing route' })
+  const products = await Product.find({ featured: true, company: 'caressa' })
+  res.status(200).json({ products, nbHits: products.length })
 }
 
 module.exports = { getAllProducts, getAllProductsStatic }
