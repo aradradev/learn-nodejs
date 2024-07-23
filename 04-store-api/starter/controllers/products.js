2b33a79ed72fc17d10c7b6f91d86a7de72bad290
Abdoul <abdoulramanediallo44@gmail.com>
2024-07-23 10:43:08 +0000

    add dynamic filter with req.query

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,10 +1,12 @@
 const Product = require('../models/product')
 
-const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: 'products testing route' })
-}
 const getAllProductsStatic = async (req, res) => {
   const products = await Product.find({ featured: true, company: 'caressa' })
+  res.status(200).json({ products, nbHits: products.length })
+}
+
+const getAllProducts = async (req, res) => {
+  const products = await Product.find(req.query)
   res.status(200).json({ products, nbHits: products.length })
 }
 
