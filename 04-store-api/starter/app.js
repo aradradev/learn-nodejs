98af00fd30e8e77b83e97fc5860322de5959db15
Abdoul <abdoulramanediallo44@gmail.com>
2024-07-23 09:34:11 +0000

    connect to DB and implement basic route

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
 require('dotenv').config()
+const connectDB = require('./db/connect')
+const productsRouter = require('./routes/products')
 const app = express()
 
 const port = process.env.PORT || 3000
@@ -12,12 +14,15 @@ app.get('/', (req, res) => {
   res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>')
 })
 
+app.use('/api/v1/products', productsRouter)
+
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
 
 const start = async () => {
   try {
     // connect to DB
+    await connectDB(process.env.MONGO_URI)
     app.listen(port, console.log(`Server is listening on port ${port}...`))
   } catch (error) {
     console.log(error)
---
+++ 04-store-api/starter/controllers/products.js
@@ -0,0 +1,8 @@
+const getAllProducts = async (req, res) => {
+  res.status(200).json({ msg: 'products route' })
+}
+const getAllProductsStatic = async (req, res) => {
+  res.status(200).json({ msg: 'products testing route' })
+}
+
+module.exports = { getAllProducts, getAllProductsStatic }
