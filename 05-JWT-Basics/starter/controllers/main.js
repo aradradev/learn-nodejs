17d8065af02acb230d2b0a1c0b3463671194d206
Abdoul <abdoulramanediallo44@gmail.com>
2024-07-24 15:38:42 +0000

    add jwt sign to get token

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -1,11 +1,16 @@
 const CustomAPIError = require('../errors/custom-error')
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
 
 const login = async (req, res) => {
   const { username, password } = req.body
   if (!username || !password) {
     throw new CustomAPIError('Something went wrong. Please provide a username or password', 400)
   }
-  res.send(`Hello, ${username}. Your password is: ${password}`)
+  const id = new Date().getTime()
+  // console.log(id)
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, { expiresIn: '30d' })
+  res.status(201).json({ msg: 'User signed in', token })
 }
 
 const dashboard = async (req, res) => {
