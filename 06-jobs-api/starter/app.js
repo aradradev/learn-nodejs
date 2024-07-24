d319a49fb5443a4904e82b7eb24c91fbcd693b2c
Abdoul <abdoulramanediallo44@gmail.com>
2024-07-24 18:39:32 +0000

    add basic models

--- 06-jobs-api/starter/app.js
+++ 06-jobs-api/starter/app.js
@@ -1,33 +1,35 @@
-require('dotenv').config();
-require('express-async-errors');
-const express = require('express');
-const app = express();
+require('dotenv').config()
+require('express-async-errors')
+const express = require('express')
+const authRouter = require('./routes/auth')
+const jobsRouter = require('./routes/jobs')
+const app = express()
 
 // error handler
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
+const notFoundMiddleware = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
+app.use(express.json())
 
-app.use(express.json());
 // extra packages
+app.use('/api/v1', authRouter)
+app.use('/api/v1', jobsRouter)
 
 // routes
 app.get('/', (req, res) => {
-  res.send('jobs api');
-});
+  res.send('jobs api')
+})
 
-app.use(notFoundMiddleware);
-app.use(errorHandlerMiddleware);
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 
 const start = async () => {
   try {
-    app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
-    );
+    app.listen(port, () => console.log(`Server is listening on port ${port}...`))
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
-};
+}
 
-start();
+start()
--- 06-jobs-api/starter/controllers/auth.js
+++ 06-jobs-api/starter/controllers/auth.js
@@ -6,4 +6,4 @@ const login = async (req, res) => {
   res.send('Login User')
 }
 
-module.exports = { register }
+module.exports = { register, login }
