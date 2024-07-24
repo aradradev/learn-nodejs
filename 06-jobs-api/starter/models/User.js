e15b42f8b9fa8513a6063edc2487f3a97bbac09f
Abdoul <abdoulramanediallo44@gmail.com>
2024-07-24 22:31:15 +0000

    seting basic user name and email

--- 06-jobs-api/starter/models/User.js
+++ 06-jobs-api/starter/models/User.js
@@ -6,6 +6,14 @@ const UserSchema = new mongoose.Schema({
     minlength: 3,
     maxlength: 32,
   },
+  email: {
+    type: String,
+    required: [true, 'Please provide email'],
+    trim: true,
+    lowercase: true,
+    unique: true,
+    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please provide valid email'],
+  },
 })
 
 module.exports = mongoose.model('User', UserSchema)
