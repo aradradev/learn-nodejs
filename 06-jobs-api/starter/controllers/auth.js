5a5e7adad3e96945354e1f77d39552f15e1fcd7a
Abdoul <abdoulramanediallo44@gmail.com>
2024-07-25 13:06:37 +0000

    add generate token instance method

--- 06-jobs-api/starter/controllers/auth.js
+++ 06-jobs-api/starter/controllers/auth.js
@@ -1,12 +1,10 @@
 const User = require('../models/User')
 const { StatusCodes } = require('http-status-codes')
-const { BadRequestError } = require('../errors')
-const jwt = require('jsonwebtoken')
 
 const register = async (req, res) => {
   const user = await User.create({ ...req.body })
-  const token = jwt.sign({ userId: user._id, name: user.name }, 'JWT_secret', { expiresIn: '30d' })
-  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token })
+  const token = user.createJWT()
+  res.status(StatusCodes.CREATED).json({ user: { name: user.getName() }, token })
 }
 
 const login = async (req, res) => {
