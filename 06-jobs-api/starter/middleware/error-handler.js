343bbf51c389b6249b78c841b5ddca5b2f243049
Abdoul <abdoulramanediallo44@gmail.com>
2024-07-28 21:36:22 +0000

    implement custom mongoose error setup

--- 06-jobs-api/starter/middleware/error-handler.js
+++ 06-jobs-api/starter/middleware/error-handler.js
@@ -1,10 +1,12 @@
 const { CustomAPIError } = require('../errors')
 const { StatusCodes } = require('http-status-codes')
 const errorHandlerMiddleware = (err, req, res, next) => {
+  let customError = {}
   if (err instanceof CustomAPIError) {
     return res.status(err.statusCode).json({ msg: err.message })
   }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+  return res.status().json({})
 }
 
 module.exports = errorHandlerMiddleware
