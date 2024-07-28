3cf72bd689ad77effa9bf0ba29715df260ca13e3
Abdoul <abdoulramanediallo44@gmail.com>
2024-07-28 22:09:52 +0000

    implement DUPLICATE ERROR

--- 06-jobs-api/starter/middleware/error-handler.js
+++ 06-jobs-api/starter/middleware/error-handler.js
@@ -1,12 +1,19 @@
 const { CustomAPIError } = require('../errors')
 const { StatusCodes } = require('http-status-codes')
 const errorHandlerMiddleware = (err, req, res, next) => {
-  let customError = {}
+  let customError = {
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || 'Something went wrong. Please try again later',
+  }
   if (err instanceof CustomAPIError) {
     return res.status(err.statusCode).json({ msg: err.message })
   }
+  if (err.code && err.code === 11000) {
+    customError.statusCode = 400
+    customError.msg = `Duplicate value ${Object.keys(err.keyValue)} fields, please provide another value`
+  }
   // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
-  return res.status().json({})
+  return res.status(customError.statusCode).json({ msg: customError.msg })
 }
 
 module.exports = errorHandlerMiddleware
