7ee395a956de8d1bdc87c3ca17595e370f2d599a
Abdoul <abdoulramanediallo44@gmail.com>
2024-07-28 22:16:07 +0000

    remove customAPIerror

--- 06-jobs-api/starter/middleware/error-handler.js
+++ 06-jobs-api/starter/middleware/error-handler.js
@@ -1,16 +1,16 @@
-const { CustomAPIError } = require('../errors')
+// const { CustomAPIError } = require('../errors')
 const { StatusCodes } = require('http-status-codes')
 const errorHandlerMiddleware = (err, req, res, next) => {
   let customError = {
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
     msg: err.message || 'Something went wrong. Please try again later',
   }
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
-  }
+  // if (err instanceof CustomAPIError) {
+  //   return res.status(err.statusCode).json({ msg: err.message })
+  // }
   if (err.code && err.code === 11000) {
     customError.statusCode = 400
-    customError.msg = `Duplicate value ${Object.keys(err.keyValue)} fields, please provide another value`
+    customError.msg = `Duplicate value entered for ${Object.keys(err.keyValue)} field, please choose another value`
   }
   // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
   return res.status(customError.statusCode).json({ msg: customError.msg })
