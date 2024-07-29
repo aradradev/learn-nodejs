cde01ad53cbae0ed0826a348201540ad5fdd7a73
Abdoul <abdoulramanediallo44@gmail.com>
2024-07-29 10:31:17 +0000

    add security middleware

--- 06-jobs-api/starter/app.js
+++ 06-jobs-api/starter/app.js
@@ -16,8 +16,13 @@ const rateLimiter = require('express-rate-limit')
 // error handler
 const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
-// extra packages
+// middlewares
+app.use(rateLimiter({}))
+
 app.use(express.json())
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 
 // routes
 app.use('/api/v1/auth', authRouter)
