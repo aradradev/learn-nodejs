93165daae86dffeff05e61cc134799d719c2e9ed
Abdoul <abdoulramanediallo44@gmail.com>
2024-07-30 22:51:48 +0000

    connect frontend static app

--- 06.5-jobster-api/starter/db/connect.js
+++ 06.5-jobster-api/starter/db/connect.js
@@ -1,12 +1,10 @@
 const mongoose = require('mongoose')
 
 const connectDB = (url) => {
-  return mongoose.connect(url, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  })
+  return mongoose
+    .connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+    .then(console.log('MongoDB Connected...'))
+    .catch((e) => `Something went wrong with the server: ${e}`)
 }
 
 module.exports = connectDB
