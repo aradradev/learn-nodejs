4fbe7e522f893a761f00571a51a7b3e373282860
Abdoul <abdoulramanediallo44@gmail.com>
2024-07-31 18:34:26 +0000

    fix positions stats

--- 06.5-jobster-api/starter/routes/jobs.js
+++ 06.5-jobster-api/starter/routes/jobs.js
@@ -6,7 +6,8 @@ const testUser = require('../middleware/test-user')
 
 router.route('/').post(testUser, createJob).get(getAllJobs)
 
-router.route('/:id').get(getJob).delete(testUser, deleteJob).patch(testUser, updateJob)
 router.route('/stats').get(showStats)
+
+router.route('/:id').get(getJob).delete(testUser, deleteJob).patch(testUser, updateJob)
 
 module.exports = router
