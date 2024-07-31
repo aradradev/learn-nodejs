7ff66cbe197fede25214d6ee7805e2754578c0a3
Abdoul <abdoulramanediallo44@gmail.com>
2024-07-31 18:05:53 +0000

    add restrict testUser middleware

--- 06.5-jobster-api/starter/routes/auth.js
+++ 06.5-jobster-api/starter/routes/auth.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
+const testUser = require('../middleware/test-user')
 const authenticatedUser = require('../middleware/authentication')
 const { register, login, updateUser } = require('../controllers/auth')
 router.post('/register', register)
 router.post('/login', login)
-router.patch('/updateUser', authenticatedUser, updateUser)
+router.patch('/updateUser', authenticatedUser, testUser, updateUser)
 
 module.exports = router
