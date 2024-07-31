ef8254a0d61047c60d6e3af8c43c14c054f3b706
Abdoul <abdoulramanediallo44@gmail.com>
2024-07-31 23:04:40 +0000

    add monthly application to the frontend

--- 06.5-jobster-api/starter/controllers/jobs.js
+++ 06.5-jobster-api/starter/controllers/jobs.js
@@ -144,8 +144,7 @@ const showStats = async (req, res) => {
       return { date, count }
     })
     .reverse()
-  console.log(monthlyApplications)
-  res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications: [] })
+  res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications })
 }
 
 module.exports = {
