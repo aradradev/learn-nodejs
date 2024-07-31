7492e26832df371cf47ceb974677d40a011f8e01
Abdoul <abdoulramanediallo44@gmail.com>
2024-07-31 18:33:53 +0000

    add jobs stats

--- 06.5-jobster-api/starter/routes/jobs.js
+++ 06.5-jobster-api/starter/routes/jobs.js
@@ -1,11 +1,12 @@
 const express = require('express')
 
 const router = express.Router()
-const { createJob, deleteJob, getAllJobs, updateJob, getJob } = require('../controllers/jobs')
+const { createJob, deleteJob, getAllJobs, updateJob, getJob, showStats } = require('../controllers/jobs')
 const testUser = require('../middleware/test-user')
 
 router.route('/').post(testUser, createJob).get(getAllJobs)
 
 router.route('/:id').get(getJob).delete(testUser, deleteJob).patch(testUser, updateJob)
+router.route('/stats').get(showStats)
 
 module.exports = router
