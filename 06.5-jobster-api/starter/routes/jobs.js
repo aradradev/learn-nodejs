7c4aea5cbd3391950ecec2422483b5d50e8c62e1
Abdoul <abdoulramanediallo44@gmail.com>
2024-07-31 18:07:14 +0000

    add restrict testUser to update jobs or create jobs

--- 06.5-jobster-api/starter/routes/jobs.js
+++ 06.5-jobster-api/starter/routes/jobs.js
@@ -1,16 +1,11 @@
 const express = require('express')
 
 const router = express.Router()
-const {
-  createJob,
-  deleteJob,
-  getAllJobs,
-  updateJob,
-  getJob,
-} = require('../controllers/jobs')
+const { createJob, deleteJob, getAllJobs, updateJob, getJob } = require('../controllers/jobs')
+const testUser = require('../middleware/test-user')
 
-router.route('/').post(createJob).get(getAllJobs)
+router.route('/').post(testUser, createJob).get(getAllJobs)
 
-router.route('/:id').get(getJob).delete(deleteJob).patch(updateJob)
+router.route('/:id').get(getJob).delete(deleteJob).patch(testUser, updateJob)
 
 module.exports = router
