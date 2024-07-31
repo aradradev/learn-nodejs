ce2ef9edc1245a81694165a9918da15021b11f89
Abdoul <abdoulramanediallo44@gmail.com>
2024-07-31 08:33:12 +0000

    fix issue with connectDB

--- 06.5-jobster-api/starter/db/connect.js
+++ 06.5-jobster-api/starter/db/connect.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose')
 
-const connectDB = (url) => {
-  return mongoose
-    .connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
-    .then(console.log('MongoDB Connected...'))
-    .catch((e) => `Something went wrong with the server: ${e}`)
+const connectDB = async (url) => {
+  try {
+    await mongoose.connect(url)
+    console.log('MongoDB Connected...')
+  } catch (error) {
+    console.log('Error connecting to MongoDB:', error)
+    throw error
+  }
 }
 
 module.exports = connectDB
