68c7d084cb623b4dca6b3295b7883f0cfa177933
Abdoul <abdoulramanediallo44@gmail.com>
2024-07-31 22:47:17 +0000

    add monthlyApplication and aggregation pipeline

--- 06.5-jobster-api/starter/controllers/jobs.js
+++ 06.5-jobster-api/starter/controllers/jobs.js
@@ -108,8 +108,31 @@ const showStats = async (req, res) => {
     { $match: { createdBy: new mongoose.Types.ObjectId(`${req.user.userId}`) } },
     { $group: { _id: '$status', count: { $sum: 1 } } },
   ])
-  console.log(stats)
-  res.status(StatusCodes.OK).json({ defaultStats: {}, monthlyApplications: [] })
+  stats = stats.reduce((acc, curr) => {
+    const { _id: title, count } = curr
+    acc[title] = count
+    return acc
+  }, {})
+  const defaultStats = {
+    pending: stats.pending || 0,
+    interview: stats.interview || 0,
+    declined: stats.declined || 0,
+  }
+
+  let monthlyApplications = await Job.aggregate([
+    { $match: { createdBy: new mongoose.Types.ObjectId(`${req.user.userId}`) } },
+    {
+      $group: {
+        _id: { year: { $year: '$createdAt' }, month: { $month: '$createdAt' } },
+        count: { $sum: 1 },
+      },
+    },
+    { $sort: { '_id.year': -1, '_id.month': -1 } },
+    { $limit: 6 },
+  ])
+
+  console.log(monthlyApplications)
+  res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications: [] })
 }
 
 module.exports = {
