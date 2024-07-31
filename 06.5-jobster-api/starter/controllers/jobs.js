8494853b2b7be90a6a623cda99bde48019fd72ab
Abdoul <abdoulramanediallo44@gmail.com>
2024-07-31 17:41:58 +0000

    add skip and limit

--- 06.5-jobster-api/starter/controllers/jobs.js
+++ 06.5-jobster-api/starter/controllers/jobs.js
@@ -34,7 +34,7 @@ const getAllJobs = async (req, res) => {
   const page = Number(req.query.page) || 1
   const limit = Number(req.query.limit) || 10
   const skip = (page - 1) * limit
-
+  result = result.skip(skip).limit(limit)
   const jobs = await result
 
   const totalJobs = await Job.countDocuments(queryObject)
