e4a3c201eea6454ddb510ee18abb49c62d29a894
Abdoul <abdoulramanediallo44@gmail.com>
2024-08-08 23:31:00 +0000

    add get all products

--- 07-file-upload/starter/controllers/productController.js
+++ 07-file-upload/starter/controllers/productController.js
@@ -3,10 +3,11 @@ const StatusCodes = require('http-status-codes')
 
 const createProduct = async (req, res) => {
   const product = await Product.create(req.body)
-  res.status(StatusCodes).json({ product })
+  res.status(StatusCodes.CREATED).json({ product })
 }
 const getAllProducts = async (req, res) => {
-  res.send('Get All Products')
+  const products = await Product.find({})
+  res.status(StatusCodes.OK).json({ products })
 }
 
 module.exports = {
