f2190155a4b30a972a7ea93013ebec7123f8da1f
Abdoul <abdoulramanediallo44@gmail.com>
2024-08-08 23:51:33 +0000

    Check error verification image

--- 07-file-upload/starter/controllers/uploadsController.js
+++ 07-file-upload/starter/controllers/uploadsController.js
@@ -1,10 +1,20 @@
 const path = require('path')
 const StatusCodes = require('http-status-codes')
+const CustomError = require('../errors')
 
 const uploadProductImage = async (req, res) => {
   console.log(req.files)
+  if (!req.files) {
+    throw new CustomError.BadRequestError('No File Uploaded')
+  }
 
   const productImage = req.files.image
+  if (!productImage.mimetype.startsWith('image')) {
+    throw new CustomError.BadRequestError('Please Upload Image')
+  }
+  if (!productImage.size > process.env.MAX_SIZE) {
+    throw new CustomError.BadRequestError('Please Upload Image Smaller 1KB')
+  }
   const imagePath = path.join(__dirname, `../public/uploads/${productImage.name}`)
   await productImage.mv(imagePath)
   return res.status(StatusCodes.OK).json({ image: { src: `uploads/${productImage.name}` } })
