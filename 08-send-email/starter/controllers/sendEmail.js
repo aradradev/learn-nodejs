7899c78fce833c96a442bd941ecdda47f1ab6106
Abdoul <abdoulramanediallo44@gmail.com>
2024-08-12 23:11:01 +0000

    implement nodemailer and ethereal

--- 08-send-email/starter/controllers/sendEmail.js
+++ 08-send-email/starter/controllers/sendEmail.js
@@ -1,4 +1,21 @@
+const nodemailer = require('nodemailer')
+
 const sendEmail = async (req, res) => {
-  res.send('send email func')
+  let testAccount = await nodemailer.createTestAccount()
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    auth: {
+      user: 'felicity.wiegand@ethereal.email',
+      pass: 'ZRFGpRE7Yn9QqQvgQQ',
+    },
+  })
+  const info = await transporter.sendMail({
+    from: '"Jalloh Abdoul" <abdoulramanediallo44@gmail.com>',
+    to: 'bar@example.com',
+    subject: 'Hello ✔',
+    html: 'Hello world?',
+  })
+  res.json(info)
 }
 module.exports = sendEmail
