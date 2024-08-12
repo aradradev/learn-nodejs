29dfb4ff54a12d9eaf903940fae44d7109850da7
Abdoul <abdoulramanediallo44@gmail.com>
2024-08-12 09:26:21 +0000

    import sendEmail in app

--- 08-send-email/starter/app.js
+++ 08-send-email/starter/app.js
@@ -4,6 +4,8 @@ require('express-async-errors')
 const express = require('express')
 const app = express()
 
+const sendEmail = require('./controllers/sendEmail')
+
 // error handler
 const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
