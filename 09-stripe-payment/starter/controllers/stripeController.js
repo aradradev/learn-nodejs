8b93b166393ad8337f26a3172c0d244f799a03e7
Abdoul <abdoulramanediallo44@gmail.com>
2024-08-13 14:42:11 +0000

    import stripe

--- 09-stripe-payment/starter/controllers/stripeController.js
+++ 09-stripe-payment/starter/controllers/stripeController.js
@@ -1,6 +1,14 @@
-const stripe = async (req, res) => {
- console.log(req.body)
-  res.send('Stripe')
+const stripe = require('stripe')(process.env.SECRET_KEY)
+const stripeController = async (req, res) => {
+  const calculateOrderAmount = () => {
+    return total_amount + shipping_fee
+  }
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: calculateOrderAmount(),
+    currency: 'usd',
+  })
+  console.log(paymentIntent)
+  res.send('stripe')
 }
 
-module.exports = stripe
+module.exports = stripeController
