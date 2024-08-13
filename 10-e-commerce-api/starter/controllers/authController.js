76b622ea4f2a59d0adddfd420832b7e8cb17b32c
Abdoul <abdoulramanediallo44@gmail.com>
2024-08-13 22:46:58 +0000

    send some string in the login

--- 10-e-commerce-api/starter/controllers/authController.js
+++ 10-e-commerce-api/starter/controllers/authController.js
@@ -1,7 +1,10 @@
 const register = async (req, res) => {
   res.send('register page')
 }
-const login = async (req, res) => {}
+
+const login = async (req, res) => {
+  res.send('login page')
+}
 const logout = async (req, res) => {}
 
 module.exports = {
