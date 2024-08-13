30444d80a21a13ca37355f06eb4caa9e5993e3a1
Abdoul <abdoulramanediallo44@gmail.com>
2024-08-13 19:06:03 +0000

    add basic express setup

--- 10-e-commerce-api/starter/app.js
+++ 10-e-commerce-api/starter/app.js
@@ -1,1 +1,36 @@
-console.log('E-Commerce API');
+// Import dependencies
+require('dotenv').config()
+require('express-async-errors')
+
+// Express
+const express = require('express')
+const app = express()
+
+// Connect to MongoDB
+const connectDB = require('./db/connect')
+
+// Import Middlewares
+const notFoundMiddleware = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
+
+app.get('/', (req, res) => {
+  res.send('<h1>E-Commerce Website</h1>')
+})
+
+// Middleware
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+
+const port = process.env.PORT || 5000
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI)
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}...`)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
