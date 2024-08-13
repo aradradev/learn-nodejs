24345d1f1771029d2eb96c12512ba95651b4d034
Abdoul <abdoulramanediallo44@gmail.com>
2024-08-13 15:08:12 +0000

    complete paymnet method

--- 09-stripe-payment/starter/controllers/stripeController.js
+++ 09-stripe-payment/starter/controllers/stripeController.js
@@ -1,13 +1,13 @@
 const stripe = require('stripe')(process.env.SECRET_KEY)
 const stripeController = async (req, res) => {
+  const { purchase, total_amount, shipping_fee } = req.body
   const calculateOrderAmount = () => {
     return total_amount + shipping_fee
   }
-  const paymentIntent = await stripe.checkout.sessions.create({
+  const paymentIntent = await stripe.paymentIntents.create({
     amount: calculateOrderAmount(),
     currency: 'usd',
   })
-  console.log(paymentIntent)
   res.json({ clientSecret: paymentIntent.client_secret })
 }
 
