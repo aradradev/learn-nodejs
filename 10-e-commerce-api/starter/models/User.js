525a8cb45e0c9af9d18fd554bc787a112f69bdbc
Abdoul <abdoulramanediallo44@gmail.com>
2024-08-13 22:25:01 +0000

    add role and basics validation in the user schema

--- 10-e-commerce-api/starter/models/User.js
+++ 10-e-commerce-api/starter/models/User.js
@@ -3,17 +3,21 @@ const mongoose = require('mongoose')
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
+    required: [true, 'Please provide name'],
+    minlength: 3,
+    maxlength: 50,
   },
   email: {
     type: String,
-    required: true,
+    required: [true, 'Please provide email'],
     unique: true,
   },
   password: {
     type: String,
-    required: true,
+    required: [true, 'Please provide password'],
+    minlength: 6,
   },
+  role: { type: String, enum: ['admin', 'user'], default: 'user' },
 })
 
 module.exports = mongoose.model('User', UserSchema)
