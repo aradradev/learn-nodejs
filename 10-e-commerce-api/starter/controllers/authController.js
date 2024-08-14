0642b0c1f726c07239010e80c9b650785f3d8106
Abdoul <abdoulramanediallo44@gmail.com>
2024-08-14 12:31:38 +0000

    check password, if does not match return 401

--- 10-e-commerce-api/starter/controllers/authController.js
+++ 10-e-commerce-api/starter/controllers/authController.js
@@ -28,11 +28,13 @@ const login = async (req, res) => {
   if (!email || !password) {
     throw new CustomError.BadRequestError('Please provide email or password')
   }
-  const user = await User.findOne({})
+  const user = await User.findOne({ email, password })
   if (!user) {
     throw new CustomError.UnauthenticatedError('Invalid Credentials')
   }
-  res.send('login page')
+  const tokenUser = { name: user.name, userId: user._id, role: user.role }
+  attachCookiesToResponse({ res, user: tokenUser })
+  res.status(StatusCodes.CREATED).json({ user: tokenUser })
 }
 
 const logout = async (req, res) => {
