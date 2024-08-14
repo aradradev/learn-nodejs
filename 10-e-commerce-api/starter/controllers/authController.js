6649cf09d5ae28cb69ce6afcdf8e2f1462415a7a
Abdoul <abdoulramanediallo44@gmail.com>
2024-08-14 12:49:22 +0000

    set expires:new Date(Date.now())

--- 10-e-commerce-api/starter/controllers/authController.js
+++ 10-e-commerce-api/starter/controllers/authController.js
@@ -44,7 +44,7 @@ const login = async (req, res) => {
 const logout = async (req, res) => {
   res.cookie('token', 'logout', {
     httpOnly: true,
-    expires: new Date(Date.now() + 5 * 1000),
+    expires: new Date(Date.now()),
   })
   res.status(StatusCodes.OK).json({ msg: 'user logged out' })
 }
