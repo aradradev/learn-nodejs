5a8a7e321bf6f4a566e064e81b9e52d8db61ddb8
Abdoul <abdoulramanediallo44@gmail.com>
2024-08-14 08:25:35 +0000

    fix unique customize error handler

--- 10-e-commerce-api/starter/controllers/authController.js
+++ 10-e-commerce-api/starter/controllers/authController.js
@@ -6,6 +6,12 @@ const { StatusCodes } = require('http-status-codes')
 const CustomError = require('../errors')
 
 const register = async (req, res) => {
+  // Check if email exists
+  const { email } = req.body
+  const isEmailExist = await User.findOne({ email })
+  if (isEmailExist) {
+    throw new CustomError.BadRequestError('Email already exists')
+  }
   const user = await User.create(req.body)
   res.status(StatusCodes.CREATED).json({ user })
 }
