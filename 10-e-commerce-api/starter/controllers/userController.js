a52fdc5a90c03d45569e3fb660303417331775f8
Abdoul <abdoulramanediallo44@gmail.com>
2024-08-16 21:07:54 +0000

    add show current user function

--- 10-e-commerce-api/starter/controllers/userController.js
+++ 10-e-commerce-api/starter/controllers/userController.js
@@ -6,7 +6,12 @@ const getSingleUser = async (res, res) => {
   res.send('get a single user')
 }
 
+const showCurrentUser = async (req, res) => {
+  res.send('show current user')
+}
+
 module.export = {
   getAllUsers,
   getSingleUser,
+  showCurrentUser,
 }
