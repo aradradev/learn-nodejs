600d520fc2beaf8dbc54dae9bac7a5c3daa80fa0
Abdoul <abdoulramanediallo44@gmail.com>
2024-08-16 21:06:41 +0000

    add get a single user function

--- 10-e-commerce-api/starter/controllers/userController.js
+++ 10-e-commerce-api/starter/controllers/userController.js
@@ -1,3 +1,12 @@
 const getAllUsers = async (req, res) => {
   res.send('get all users')
 }
+
+const getSingleUser = async (res, res) => {
+  res.send('get a single user')
+}
+
+module.export = {
+  getAllUsers,
+  getSingleUser,
+}
