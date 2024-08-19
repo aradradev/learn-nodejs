9e8612c82aeb0bce233dab50e3bea95444ea86dc
Abdoul <abdoulramanediallo44@gmail.com>
2024-08-19 17:36:12 +0000

    add req.user

--- 10-e-commerce-api/starter/middleware/authentication.js
+++ 10-e-commerce-api/starter/middleware/authentication.js
@@ -1,5 +1,5 @@
 const CustomError = require('../errors')
-const isTokenValid = require('../utils')
+const { isTokenValid } = require('../utils')
 
 const authenticateUser = async (req, res, next) => {
   const token = req.signedCookies.token
@@ -8,7 +8,7 @@ const authenticateUser = async (req, res, next) => {
   }
   try {
     const payload = isTokenValid({ token })
-    console.log(payload)
+    req.user = { name: user.name, userId: user._id, role: user.role }
     next()
   } catch (error) {
     throw new CustomError.UnauthenticatedError('Authentication Invalid')
