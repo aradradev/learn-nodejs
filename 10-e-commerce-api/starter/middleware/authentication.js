9fe99e5ac0eacb333066e0d808255daf09434ce3
Abdoul <abdoulramanediallo44@gmail.com>
2024-08-19 18:29:41 +0000

    Authorize Permissions Complete

--- 10-e-commerce-api/starter/middleware/authentication.js
+++ 10-e-commerce-api/starter/middleware/authentication.js
@@ -17,6 +17,9 @@ const authenticateUser = async (req, res, next) => {
 const authorizePermissions = (...roles) => {
   return (req, res, next) => {
     console.log(...roles)
+    if (!roles.includes(req.user.role)) {
+      throw new CustomError.UnauthorizedError('You are not authorized to access this route')
+    }
     next()
   }
 }
