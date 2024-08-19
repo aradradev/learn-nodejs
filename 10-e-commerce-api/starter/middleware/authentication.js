41bb2c224c9e64a40b56024f9136ff2c84969725
Abdoul <abdoulramanediallo44@gmail.com>
2024-08-19 17:26:35 +0000

    add payload

--- 10-e-commerce-api/starter/middleware/authentication.js
+++ 10-e-commerce-api/starter/middleware/authentication.js
@@ -1,14 +1,18 @@
-const customError = require('../errors')
+const CustomError = require('../errors')
 const isTokenValid = require('../utils')
 
 const authenticateUser = async (req, res, next) => {
   const token = req.signedCookies.token
   if (!token) {
-    console.log('error, no token present')
-  } else {
-    console.log('token present')
+    throw new CustomError.UnauthenticatedError('Authentication invalid')
   }
-  next()
+  try {
+    const payload = isTokenValid({ token })
+    console.log(payload)
+    next()
+  } catch (error) {
+    throw new CustomError.UnauthenticatedError('Authentication Invalid')
+  }
 }
 
 module.exports = authenticateUser
