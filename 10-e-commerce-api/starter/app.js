9197b2be6a647c1010311f18a645aaa1182ed846
Abdoul <abdoulramanediallo44@gmail.com>
2024-08-20 12:23:10 +0000

    import router to app

--- 10-e-commerce-api/starter/app.js
+++ 10-e-commerce-api/starter/app.js
@@ -13,6 +13,7 @@ const cookieParser = require('cookie-parser')
 // authRouter
 const authRouter = require('./routes/authRoutes')
 const userRouter = require('./routes/userRoutes')
+const productRouter = require('./routes/productRoutes')
 // Connect to MongoDB
 const connectDB = require('./db/connect')
 
@@ -31,9 +32,10 @@ app.get('/', (req, res) => {
   res.send('<h1>E-Commerce API</h1>')
 })
 
-// Routes
+// Routes middleware
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/users', userRouter)
+app.use('/api/v1/products', productRouter)
 
 // Middleware
 app.use(notFoundMiddleware)
--- 10-e-commerce-api/starter/routes/productRoutes.js
+++ 10-e-commerce-api/starter/routes/productRoutes.js
@@ -9,3 +9,14 @@ const {
   deleteProduct,
   uploadImage,
 } = require('../controllers/productController')
+
+// get all product
+router.route('/').get(getAllProducts).post(createProduct)
+
+// upload image
+router.route('/uploadImage').post(uploadImage)
+
+// get single route with id params
+router.route('/:id').get(getSingleProduct).patch(updateProduct).delete(deleteProduct)
+
+module.exports = router
