d5627d61837c37978a8f552a4b87c3b035ccf600
Abdoul <abdoulramanediallo44@gmail.com>
2024-08-20 11:55:53 +0000

    add user ref and timestamps

--- 10-e-commerce-api/starter/models/Product.js
+++ 10-e-commerce-api/starter/models/Product.js
@@ -1,62 +1,68 @@
 const mongoose = require('mongoose')
-const ProductSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'Please provide product name'],
-    trim: true,
-    maxlength: [100, 'Name cannot be more than 100 characters'],
-  },
-  price: {
-    type: Number,
-    required: [true, 'Please provide product price'],
-    default: 0,
-  },
-  description: {
-    type: String,
-    required: [true, 'Please provide product description'],
-    trim: true,
-    maxlength: [1000, 'Description cannot be more than 1000 characters'],
-  },
-  image: {
-    type: String,
-    default: '/uploads/example.jpeg',
-  },
-  category: {
-    type: String,
-    required: [true, 'Please provide product category'],
-    enum: ['office', 'kitchen', 'bedroom'],
-  },
-  company: {
-    type: String,
-    required: [true, 'Please provide company name'],
-    enum: {
-      values: ['ikea', 'liddy', 'marcos'],
-      message: '{VALUE} is not supported',
+const ProductSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Please provide product name'],
+      trim: true,
+      maxlength: [100, 'Name cannot be more than 100 characters'],
+    },
+    price: {
+      type: Number,
+      required: [true, 'Please provide product price'],
+      default: 0,
+    },
+    description: {
+      type: String,
+      required: [true, 'Please provide product description'],
+      trim: true,
+      maxlength: [1000, 'Description cannot be more than 1000 characters'],
+    },
+    image: {
+      type: String,
+      default: '/uploads/example.jpeg',
+    },
+    category: {
+      type: String,
+      required: [true, 'Please provide product category'],
+      enum: ['office', 'kitchen', 'bedroom'],
+    },
+    company: {
+      type: String,
+      required: [true, 'Please provide company name'],
+      enum: {
+        values: ['ikea', 'liddy', 'marcos'],
+        message: '{VALUE} is not supported',
+      },
+    },
+    colors: {
+      type: [String],
+      required: true,
+    },
+    featured: {
+      type: Boolean,
+      default: false,
+    },
+    freeShipping: {
+      type: Boolean,
+      default: false,
+    },
+    inventory: {
+      type: Number,
+      required: true,
+      default: 15,
+    },
+    averageRating: {
+      type: Number,
+      default: 0,
+    },
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+      required: true,
     },
   },
-  colors: {
-    type: [String],
-    required: true,
-  },
-  featured: {
-    type: Boolean,
-    default: false,
-  },
-  freeShipping: {
-    type: Boolean,
-    default: false,
-  },
-  inventory: {
-    type: Number,
-    required: true,
-    default: 15,
-  },
-  averageRating: {
-    type: Number,
-    default: 0,
-  },
-  user: mongoose.Types.ObjectId('User'),
-  timestamps: true,
-})
+  { timestamps: true },
+)
 
 module.exports = mongoose.model('Product', ProductSchema)
