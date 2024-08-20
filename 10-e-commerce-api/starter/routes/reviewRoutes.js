bcd0734dcd34db838a68acf9b4b2456de0dbfa87
Abdoul <abdoulramanediallo44@gmail.com>
2024-08-20 21:43:46 +0000

    add authenticate user and authorize permission

---
+++ 10-e-commerce-api/starter/routes/reviewRoutes.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const router = express.Router()
+
+// import all review controllers
+const {
+  createReview,
+  getAllReviews,
+  getSingleReview,
+  updateReview,
+  deleteReview,
+} = require('../controllers/reviewController')
+
+// import authenticate middleware
+const { authenticateUser, authorizePermissions } = require('../middleware/authentication')
+
+router
+  .route('/')
+  .post([authenticateUser, authorizePermissions('admin')], createReview)
+  .get(getAllReviews)
+
+router
+  .route('/:id')
+  .get(getSingleReview)
+  .patch([authenticateUser, authorizePermissions('admin')], updateReview)
+  .delete([authenticateUser, authorizePermissions('admin')], deleteReview)
+
+module.exports = router
