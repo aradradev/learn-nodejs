eb8df33b1b4106cca0223ef3a5d0ecfda90a1cc0
Abdoul <abdoulramanediallo44@gmail.com>
2024-08-23 21:37:09 +0000

    create Order.js in models folder

--- 10-e-commerce-api/starter/models/Order.js
+++ 10-e-commerce-api/starter/models/Order.js
@@ -1,5 +1,38 @@
+const { required } = require('joi')
 const mongoose = require('mongoose')
 
-const OrderSchema = new mongoose.Schema({})
+const OrderSchema = new mongoose.Schema(
+  {
+    tax: {
+      type: Number,
+      required: true,
+    },
+    shippingFee: {
+      type: Number,
+      required: true,
+    },
+    subtotal: {
+      type: Number,
+      required: true,
+    },
+    total: {
+      type: Number,
+      required: true,
+    },
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+    },
+    clientSecret: {
+      type: String,
+      required: true,
+    },
+    paymentId: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true },
+)
 
 module.exports = mongoose.model('Order', OrderSchema)
