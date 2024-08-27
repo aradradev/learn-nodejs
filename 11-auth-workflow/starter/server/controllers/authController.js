b0c7017061dcfd7e95593a91b2e402b89f658636
Abdoul <abdoulramanediallo44@gmail.com>
2024-08-27 10:10:40 +0000

    Update login contoroller

--- 11-auth-workflow/starter/server/controllers/authController.js
+++ 11-auth-workflow/starter/server/controllers/authController.js
@@ -37,6 +37,11 @@ const login = async (req, res) => {
   if (!isPasswordCorrect) {
     throw new CustomError.UnauthenticatedError('Invalid Credentials')
   }
+
+  if (!user.isVerified) {
+    throw new CustomError.UnauthenticatedError('Please verify your email')
+  }
+
   const tokenUser = createTokenUser(user)
   attachCookiesToResponse({ res, user: tokenUser })
 
