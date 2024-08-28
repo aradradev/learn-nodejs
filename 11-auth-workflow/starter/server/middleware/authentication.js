5e547515804259ce3d7cce234ec3668d54070028
Abdoul <abdoulramanediallo44@gmail.com>
2024-08-28 13:28:08 +0000

    refactor the authentication with accessTokenJWT

--- 11-auth-workflow/starter/server/middleware/authentication.js
+++ 11-auth-workflow/starter/server/middleware/authentication.js
@@ -1,34 +1,28 @@
-const CustomError = require('../errors');
-const { isTokenValid } = require('../utils');
+const CustomError = require('../errors')
+const { isTokenValid } = require('../utils')
 
 const authenticateUser = async (req, res, next) => {
-  const token = req.signedCookies.token;
-
-  if (!token) {
-    throw new CustomError.UnauthenticatedError('Authentication Invalid');
-  }
+  const { accessTokenJWT, refreshTokenJWT } = req.signedCookies
 
   try {
-    const { name, userId, role } = isTokenValid({ token });
-    req.user = { name, userId, role };
-    next();
+    const { name, userId, role } = isTokenValid({ token })
+    req.user = { name, userId, role }
+    next()
   } catch (error) {
-    throw new CustomError.UnauthenticatedError('Authentication Invalid');
+    throw new CustomError.UnauthenticatedError('Authentication Invalid')
   }
-};
+}
 
 const authorizePermissions = (...roles) => {
   return (req, res, next) => {
     if (!roles.includes(req.user.role)) {
-      throw new CustomError.UnauthorizedError(
-        'Unauthorized to access this route'
-      );
+      throw new CustomError.UnauthorizedError('Unauthorized to access this route')
     }
-    next();
-  };
-};
+    next()
+  }
+}
 
 module.exports = {
   authenticateUser,
   authorizePermissions,
-};
+}
