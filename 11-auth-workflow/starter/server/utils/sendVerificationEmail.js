5b8baec727a3d886422845db041f17f4144eef5b
Abdoul <abdoulramanediallo44@gmail.com>
2024-08-28 10:36:15 +0000

    fix issue

--- 11-auth-workflow/starter/server/utils/sendVerificationEmail.js
+++ 11-auth-workflow/starter/server/utils/sendVerificationEmail.js
@@ -1,7 +1,7 @@
 const sendEmail = require('./sendEmail')
 
 const sendVerificationEmail = async ({ name, email, verificationToken, origin }) => {
-  const verifyEmail = `${origin}/user/verify-token?token=${verificationToken}&email=${email}`
+  const verifyEmail = `${origin}/user/verify-email?token=${verificationToken}&email=${email}`
 
   const message = `<p>Please confirm your email by clicking on the following link:<a href="${verifyEmail}">Verify Email</a> </p>`
 
